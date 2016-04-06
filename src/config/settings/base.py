173d6481f13b4a96d9d801300284f9ddcad11439
Nikita Grishko <gr1n@protonmail.com>
2016-04-06 21:51:44 +0300

    Added database connection

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
 ####################
+# DATABASE         #
+####################
+
+DATABASES = {
+    'default': {
+        'BACKEND': 'wuffi.core.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': None,
+        'PASSWORD': None,
+        'DATABASE': 'postgres',
+        'POOLSIZE': 10,
+    },
+}
+
+####################
 # CACHE            #
 ####################
 
