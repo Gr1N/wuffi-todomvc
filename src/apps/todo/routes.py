0fe4f5fb2600c9e05965bd4f51576e41a8914e5c
Nikita Grishko <ng@gojuno.com>
2016-04-12 18:27:45 +0300

    Added todos list API method

--- src/apps/todo/routes.py
+++ src/apps/todo/routes.py
@@ -6,4 +6,5 @@ from apps.todo import views
 
 
 router = UrlDispatcher()
+router.add_route('*', '/', views.TodosView)
 router.add_route('*', '/{id}', views.TodoView)
--- src/apps/todo/views.py
+++ src/apps/todo/views.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from wuffi.views.generic import RetrieveView
+from wuffi.views.generic import ListView, RetrieveView
 
 from apps.todo import tables
 
 __all__ = (
+    'TodosView',
     'TodoView',
 )
+
+
+class TodosView(ListView):
+    select = (
+        tables.item,
+    )
+    order_by = tables.item.c.id
 
 
 class TodoView(RetrieveView):
