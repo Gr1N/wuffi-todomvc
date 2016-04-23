bbe00f06c2ed3300cc9c47a50d31d62f9b18cb4f
Nikita Grishko <gr1n@protonmail.com>
2016-04-23 18:24:26 +0300

    Updated configuration

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 ####################
 # DATABASE         #
 ####################
@@ -24,4 +29,14 @@ CACHES = {
     'default': {
         'BACKEND': 'wuffi.core.cache.backends.redis',
     },
+}
+
+####################
+# TEMPLATES        #
+####################
+
+TEMPLATES = {
+    # 'DIRS': [
+    #     'apps/todo/templates/',
+    # ],
 }
